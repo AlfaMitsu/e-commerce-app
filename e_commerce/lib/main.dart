dc14dae61dc04b46aa4e0e2c2e6e904ff2208c5a
AlfaMitsu <hadjiusopalfasheer@gmail.com>
2024-10-12 13:15:14 +0800

    update

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
               },
             );
           }
-          return null; // Fallback in case of an unknown route
+          return null;
         },
         routes: {
           '/': (context) => const HomeView(),
